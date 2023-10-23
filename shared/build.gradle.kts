a9b12bd7f9baaddfc2ba80089fdbd397f4822a01
dn0ne <mmmdanone@gmail.com>
2023-10-23 14:50:58 +0300

    Delete Realm plugin, that was causing build to fail

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.compose")
     id("dev.icerock.mobile.multiplatform-resources")
-    id("io.realm.kotlin") version "1.11.0"
 }
 
 val mokoResourcesVersion = extra["moko.resources.version"] as String
