cb467e2075227876d5ce46ef4cfb850bb6d9132f
dn0ne <mmmdanone@gmail.com>
2023-11-30 11:34:10 +0200

    Add DecimalFormatter class that filters user's decimal input

---
+++ shared/src/commonMain/kotlin/com.dn0ne.moneymate/util/DecimalFormatter.kt
@@ -0,0 +1,29 @@
+package com.dn0ne.moneymate.util
+
+object DecimalFormatter {
+
+    private const val decimalSeparator = '.'
+
+    fun cleanup(input: String): String {
+
+        if (input.matches("\\D".toRegex())) return ""
+        if (input.matches("0+".toRegex())) return "0"
+
+        val sb = StringBuilder()
+
+        var hasDecimalSep = false
+
+        for (char in input) {
+            if (char.isDigit()) {
+                sb.append(char)
+                continue
+            }
+            if (char == decimalSeparator && !hasDecimalSep && sb.isNotEmpty()) {
+                sb.append(char)
+                hasDecimalSep = true
+            }
+        }
+
+        return sb.toString()
+    }
+}
