038ce86a194fd2e43b7caa6f46f8cb108c146c0f
dn0ne <mmmdanone@gmail.com>
2024-01-25 17:37:45 +0200

    Minor changes

--- shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/extensions/FloatExtensions.kt
+++ shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/extensions/FloatExtensions.kt
@@ -1,7 +1,14 @@
 package com.dn0ne.moneymate.app.extensions
 
+/**
+ * Returns float converted into string in format "\d+.\d{[scale]}"
+ */
 fun Float.toStringWithScale(scale: Int): String {
     var result = this.toString()
+
+    if (result.any { it.lowercaseChar() == 'e' }) {
+        return "Too much"
+    }
 
     var scaleDifference = result.takeLastWhile { it != '.' }.length - scale
     val smallerScaleCase = scaleDifference < 0
