538f62c45902afc5087ea94f26bf58fa0ec1f8d5
dn0ne <mmmdanone@gmail.com>
2024-01-25 17:42:05 +0200

    Add splash screen, enable edge to edge, other minor changes

--- androidApp/src/androidMain/kotlin/com/dn0ne/moneymate/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/dn0ne/moneymate/MainActivity.kt
@@ -2,20 +2,27 @@ package com.dn0ne.moneymate
 
 import App
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
 
         val darkTheme =
             resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+        val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
         setContent {
             App(
                 darkTheme = darkTheme,
-                dynamicColor = true
+                dynamicColor = dynamicColor
             )
         }
     }
