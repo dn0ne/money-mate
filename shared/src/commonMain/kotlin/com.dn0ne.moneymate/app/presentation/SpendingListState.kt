9f6ce1ee91223847b237f3131ad5e21740863f90
dn0ne <mmmdanone@gmail.com>
2024-01-25 17:26:52 +0200

    Add new fields

--- shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/presentation/SpendingListState.kt
+++ shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/presentation/SpendingListState.kt
@@ -1,19 +1,42 @@
 package com.dn0ne.moneymate.app.presentation
 
 import com.dn0ne.moneymate.app.domain.Category
+import com.dn0ne.moneymate.app.domain.Settings
 import com.dn0ne.moneymate.app.domain.ShoppingItem
 import com.dn0ne.moneymate.app.domain.Spending
+import kotlinx.datetime.LocalDate
 
+/**
+ * App state class
+ */
 data class SpendingListState(
     val spendings: List<Spending> = emptyList(),
     val categories: List<Category> = emptyList(),
+    val settings: Settings = Settings(),
+    val isDataLoaded: Boolean = false,
+
     val selectedSpending: Spending? = null,
+    val isSelectedSpendingSheetOpen: Boolean = false,
+
     val isAddSpendingSheetOpen: Boolean = false,
     val areSpendingDetailsAdded: Boolean = false,
     val detailsType: Int = 0,
-    val isSelectedSpendingSheetOpen: Boolean = false,
-    val categoryError: String? = null,
-    val amountError: String? = null,
-    val shortDescriptionError: String? = null,
-    val shoppingListError: Map<ShoppingItem, Pair<Boolean, Boolean>>? = null
+
+    val categoryError: Boolean? = null,
+    val amountError: Boolean? = null,
+    val shortDescriptionError: Boolean? = null,
+    val shoppingListError: Map<ShoppingItem, Pair<Boolean, Boolean>>? = null,
+
+    val showAddCategoryDialog: Boolean = false,
+
+    val categoryNameError: Boolean? = null,
+    val categoryIconNameError: Boolean? = null,
+
+    val isSummarySheetOpen: Boolean = false,
+    val summaryTimePeriod: ClosedRange<LocalDate>? = null,
+
+    val isSettingsSheetOpen: Boolean = false,
+    val showBudgetAmountChangeDialog: Boolean = false,
+    val showBudgetPeriodChangeDialog: Boolean = false,
+    val showPeriodStartChangeDialog: Boolean = false,
 )
