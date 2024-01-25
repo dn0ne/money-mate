0837928a3e723a2ea85cab288864beca68b22028
dn0ne <mmmdanone@gmail.com>
2024-01-25 17:21:36 +0200

    Change RealmInstant to LocalDate converting logic

--- shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/extensions/RealmInstantExtensions.kt
+++ shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/extensions/RealmInstantExtensions.kt
@@ -1,8 +1,12 @@
 package com.dn0ne.moneymate.app.extensions
 
+import io.realm.kotlin.internal.toDuration
 import io.realm.kotlin.types.RealmInstant
 import kotlinx.datetime.LocalDate
 
+/**
+ * Returns RealmInstant converted to LocalDate
+ */
 fun RealmInstant.toLocalDate(): LocalDate {
-    return LocalDate.fromEpochDays((epochSeconds / 60 / 60 / 24).toInt())
+    return LocalDate.fromEpochDays(this.toDuration().inWholeDays.toInt())
 }
