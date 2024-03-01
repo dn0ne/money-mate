aa47749cb7c9bfcc69bfe44ce947aaac5b6e8c2b
dn0ne <mmmdanone@gmail.com>
2024-03-01 12:42:36 +0200

    Delete unused extension

--- shared/src/commonMain/kotlin/com.dn0ne.moneymate/app/domain/extensions/StringExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.dn0ne.moneymate.app.domain.extensions
-
-/**
- * Returns string where first character is uppercase and others are lowercase.
- * Use only for words.
- */
-fun String.capitalize(): String {
-    return this.lowercase().replaceFirstChar { it.uppercase() }
-}
