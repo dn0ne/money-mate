56feaaac0dd2988f2541e1e882a41a74b004b2b9
dn0ne <mmmdanone@gmail.com>
2024-03-03 11:33:35 +0200

    Add Koin initialization

---
+++ androidApp/src/androidMain/kotlin/com/dn0ne/moneymate/MainApplication.kt
@@ -0,0 +1,17 @@
+package com.dn0ne.moneymate
+
+import android.app.Application
+import com.dn0ne.moneymate.app.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
+    }
+}
